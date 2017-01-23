12173c6477729336ec204fd20082e0ac3545792f
ravenusmc <mcuddy77@gmail.com>
2017-01-23 11:38:43 -0500

    working on displaying data

--- draft.py
+++ draft.py
@@ -2,6 +2,9 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from datetime import datetime
+from collections import Counter
+import operator
+import collections
 
 
 data = pd.read_csv('database.csv')
@@ -9,24 +12,79 @@ data = pd.read_csv('database.csv')
 black_deaths = []
 dates = []
 start = 0
+prev_start = -1
 black_count = 0
+
+# while start < 12:
+#     date = datetime.strptime(data.iat[start, 2], "%Y-%m-%d")
+#     previous_date = datetime.strptime(data.iat[prev_start, 2], "%Y-%m-%d")
+#     start += 1
+#     prev_start += 1
+
 
 while start < 1948:
     date = datetime.strptime(data.iat[start, 2], "%Y-%m-%d")
+    previous_date = datetime.strptime(data.iat[prev_start, 2], "%Y-%m-%d")
     race = data.iat[start, 7]
     if race == 'B':
         black_count += 1
         black_deaths.append(black_count)
         dates.append(date)
     start += 1
+    prev_start += 1
 
-fig = plt.figure(dpi=128, figsize=(10,6))
-plt.plot(dates, black_deaths, linewidth=2, c="red")
-plt.title("Deaths", fontsize=24)
-plt.xlabel('Date', fontsize=16)
-fig.autofmt_xdate()
-plt.ylabel("Deaths", fontsize=16)
-plt.show()
+
+# print(dates[1])
+# previous_date = datetime.strptime(data.iat[0, 2], "%Y-%m-%d")
+# print(previous_date)
+
+#This code gets me the count at the specific location that I enter.
+# print(dates.count(dates[2]))
+values = Counter(dates)
+od = collections.OrderedDict(sorted(values.items()))
+#length of od is 355
+print(od)
+
+# date_count = 1
+# previous = 0
+# death_count = []
+# while previous < 100:
+#     if datetime.strptime(data.iat[date_count, 2], "%Y-%m-%d") != datetime.strptime(data.iat[previous, 2], "%Y-%m-%d"):
+#         death_count.append(dates.count(dates[date_count]))
+#     date_count += 1
+#     previous += 1
+# print(death_count)
+
+
+
+# container = []
+# test_count = 0
+# search_count = 0
+# search_value = dates[test_count]
+# print(search_value)
+# for date in dates:
+#     search_value = dates[test_count]
+#     print(search_value)
+#     test_count += 1
+#     if search_value == dates[test_count]:
+#         search_count += 1
+#         container.append(search_count)
+# print(container)
+
+
+
+
+# fig = plt.figure(dpi=128, figsize=(10,6))
+# plt.plot(dates, black_deaths, linewidth=2, c="red")
+# plt.title("Deaths", fontsize=24)
+# plt.xlabel('Date', fontsize=16)
+# fig.autofmt_xdate()
+# plt.ylabel("Deaths", fontsize=16)
+# plt.show()
+
+
+
+
 
 ##### Scrap #######
 
